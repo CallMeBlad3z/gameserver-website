d84025f09a09eabbe68aca6b1b7fcac918a43767
Blad3z <santtu366@live.com>
2024-04-22 23:27:57 +0300

    Update page.tsx

--- community-website/src/app/about/page.tsx
+++ community-website/src/app/about/page.tsx
@@ -1,10 +1,12 @@
 export default function AboutPage() {
   return (
-    <div>
-      <h1>About Pelitupa</h1>
-      <p>
-        Pelitupa is a gaming community that offers servers and events for players.
-      </p>
+    <div className="bg-red-dark">
+      <div className="pt-28">
+        <h1>About Pelitupa</h1>
+        <p>
+          Pelitupa is a gaming community that offers servers and events for players.
+        </p>
+      </div>
     </div>
   );
 }
