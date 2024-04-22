ba603608642b312caa79325f3cda401339cb012c
Blad3z <santtu366@live.com>
2024-04-22 23:14:42 +0300

    Better fix for header

--- community-website/src/components/hero.tsx
+++ community-website/src/components/hero.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 interface HeroProps {
     imgData1: StaticImageData; // The first image displayed in the background
     imgData2: StaticImageData; // The second image displayed in the background
-    imgAlt: string;
+    imgAlt: string; 
     title: string;
 }
 // This is the Hero component. It is displayed at the top of the page and contains a title and a background image that changes every 5 seconds.
@@ -17,21 +17,21 @@ export default function Hero(props: HeroProps) {
 
     useEffect(() => {
         const timer = setInterval(() => {
-            if (opacity1 === 1) {
+            if (opacity1 === 1) {                // If the first image is displayed, change the opacity of the second image to 1 and the opacity of the first image to 0
                 setOpacity1(0);
                 setOpacity2(1);
             } else {
                 setOpacity1(1);
                 setOpacity2(0);
             }
-        }, 5000);
-        return () => clearInterval(timer); // Clear the interval when the component is unmounted
+        }, 5000);                               // Change the background image every 5 seconds
+        return () => clearInterval(timer);      // Clear the interval when the component is unmounted
     }, [opacity1]);
 
 return (
-    <div className="flex items-center justify-center relative h-dvh w-dvh">
-        <div className="absolute -z-30 inset-0">
-            <div className="absolute h-full w-full">
+    <div className="flex items-center justify-center relative pointer-events-none h-dvh w-dvh">
+        <div className="absolute -z-10 inset-0">
+            <div className="absolute layout-fill h-full w-full ease-in transition-opacity duration-1000 opacity-100">
                 <Image 
                     src={props.imgData1}
                     alt={props.imgAlt}
