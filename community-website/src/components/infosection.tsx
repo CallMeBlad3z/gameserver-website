7d4108aafb84b4c9d86c7571931c7f07912c6cba
Blad3z <santtu366@live.com>
2024-04-22 23:16:25 +0300

    Cleaned code and and switched to a better method (WIP)

--- community-website/src/components/infosection.tsx
+++ community-website/src/components/infosection.tsx
@@ -1,19 +1,19 @@
 export default function InfoSection() {
     return (
         <section className="bg-red-dark">
-            <div className="container relative flex flex-wrap items-center justify-between mx-auto pb-52 pt-28">
-                <div className="flex xs:flex-col sm:flex-col md:flex-col lg:flex-row xs:space-y-4  lg:space-y-0 lg:space-x-24">
-                    <div className="container p-4 xs:p-5 sm:p-10 bg-red shadow-2xl rounded">
+            <div className="container items-center mx-auto pb-52 pt-28">
+                <div className="container flex flex-wrap gap-14">
+                    <div className="flex-1 rounded p-4 xs:p-5 sm:p-10 bg-red shadow-2xl ">
                         <h2 className="text-white text-4xl text-center pb-4">Palvelimet</h2>
-                        <p className="text-white text-2xl mx-auto">Pyöritämme tällä hetkellä 7 palvelinta TF2:sa ja CS2:sa yhteensä. Tulevaisuudessa tulemme laajentamaan uusiin pelimuotoihin ja jopa mahdollisesti muihin peleihin.</p>
+                        <p className="text-white text-2xl">Pyöritämme tällä hetkellä 7 palvelinta TF2:sa ja CS2:sa yhteensä. Tulevaisuudessa tulemme laajentamaan uusiin pelimuotoihin ja jopa mahdollisesti muihin peleihin.</p>
                     </div>
-                    <div className="container p-4 xs:p-5 sm:p-10 bg-red shadow-2xl rounded">
+                    <div className="flex-1 rounded p-4 xs:p-5 sm:p-10 bg-red shadow-2xl">
                         <h2 className="text-white text-4xl text-center pb-4">Tietoa meistä</h2>
-                        <p className="text-white text-2xl mx-auto">Pelitupa on suomalainen peliyhteisö, joka on perustettu vuonna 2018. Tavoitteenamme on tarjota pelaajille turvallinen ja viihtyisä ympäristö, jossa he voivat pitää hauskaa yhdessä muiden pelaajien kanssa.</p>
+                        <p className="text-white text-2xl">Pelitupa on suomalainen peliyhteisö, joka on perustettu vuonna 2018. Tavoitteenamme on tarjota pelaajille turvallinen ja viihtyisä ympäristö, jossa he voivat pitää hauskaa yhdessä muiden pelaajien kanssa.</p>
                     </div>
-                    <div className="container p-4 xs:p-5 sm:p-10 bg-red shadow-2xl rounded">
-                        <h2 className="text-white text-4xl text-center pb-4">Discord</h2>
-                        <p className="text-white text-2xl mx-auto">Liity Pelituvan Discord-palvelimelle ja keskustele muiden pelaajien kanssa! Discord on ilmainen ääni- ja tekstipalvelin, joka on suunniteltu erityisesti pelaajille.</p>
+                    <div className="flex-1 rounded p-4 xs:p-5 sm:p-10 bg-red shadow-2xl">
+                        <h2 className="text-white sm:text-2xl md:text-3xl lg:text-4xl text-center pb-4">Discord</h2>
+                        <p className="text-white md:text-1xl lg:text-2xl">Liity Pelituvan Discord-palvelimelle ja keskustele muiden pelaajien kanssa! Discord on ilmainen ääni- ja tekstipalvelin, joka on suunniteltu erityisesti pelaajille.</p>
                     </div>
                 </div>
             </div>
