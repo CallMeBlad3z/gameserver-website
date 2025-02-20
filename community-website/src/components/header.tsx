8cdec1988f9812ff50e0fb51efe811cebb37d6e8
Blad3z <santtu366@live.com>
2025-02-20 20:30:05 +0200

    Enhance header component with Discord icon and improved layout

--- community-website/src/components/header.tsx
+++ community-website/src/components/header.tsx
@@ -1,21 +1,27 @@
-import Image from "next/image";
 import discordLogo from "/public/discord.png";
 import Link from "next/link";
+import { FaDiscord } from "react-icons/fa";
+
 export default function Header() {
   return (
-    <div className="w-full absolute">
-        <nav className="container relative flex flex-wrap items-center justify-between mx-auto p-8 bg-red-lighter shadow-2xl rounded-b">
-            <Link href="/" className="font-bond text-3xl">
+    <div className="flex w-full absolute">
+        <nav className="container max-w-7xl text-white flex flex-wrap items-center justify-between mx-auto p-8 bg-red shadow-2xl rounded-b">
+            <Link href="/" className="text-3xl">
                 Pelitupa.fi
             </Link>
 
-            <div className="space-x-4 text-xl">
+            <div className="space-x-6 lg:text-xl">
                 <Link href="/servers">Palvelimet</Link>
+                <Link href="/commands">Komennot</Link>
                 <Link className="mr-100" href="/about">Tietoa meistä</Link>
-
-                <Link className="space-x-4 text-xl"
-                  //style={{ backgroundColor: '#40444b' }} inline-flex items-center justify-center h-8 w-8 rounded-full bg-red-re
-                  href="https://discord.gg/3rTUMdn">Discord</Link>
+            </div>
+            
+            <div>
+                <div className="flex items-center justify-center bg-white rounded-full shadow-lg w-8 h-8">
+                  <Link className="space-x-4 text-xl"
+                    //style={{ backgroundColor: '#40444b' }} inline-flex items-center justify-center h-8 w-8 rounded-full bg-red-re
+                    href="https://discord.gg/3rTUMdn"><FaDiscord className="fill-black w-6 h-6 fill-red"/></Link>
+                </div>
             </div>
         </nav>
     </div>
