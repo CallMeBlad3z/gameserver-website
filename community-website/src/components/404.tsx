a9a306afef4012f203e1b77ceda341c3d95d3bf1
Blad3z <santtu366@live.com>
2025-02-20 20:28:04 +0200

    Improve 404 page styling and background color

--- community-website/src/components/404.tsx
+++ community-website/src/components/404.tsx
@@ -4,11 +4,11 @@ import jantteriImg from '/public/jantteri_miettii.jpg';
 // This is a 404 page component. It is displayed when the user tries to access a page that does not exist. (WIP)
 export default function Nowhere() {
     return (
-        <div className="flex items-center justify-center h-screen bg-red">
-            <div className="p-8 shadow-2xl rounded bg-red-lightest">
+        <div className="flex items-center justify-center h-screen bg-red-dark">
+            <div className="p-8 shadow-2xl rounded bg-red">
                 <div className="flex flex-row items-center justify-center space-x-10">
                     <div className="flex flex-col items-center justify-center">
-                        <h1 className="text-4xl font-bold ">404 - Sivua ei löytynyt</h1>
+                        <h1 className="text-4xl font-bold text-white ">404 - Sivua ei löytynyt</h1>
                     </div>
                     <Image
                         src={jantteriImg}
