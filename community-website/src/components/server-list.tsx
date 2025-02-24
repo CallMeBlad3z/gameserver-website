889d7a1a79a699a4fc5fcafc9af2d787d3f4bef7
Blad3z <santtu366@live.com>
2025-02-24 19:27:28 +0200

    Refactor server list component to accept dynamic server data and enhance layout with improved styling and hover effects

--- community-website/src/components/server-list.tsx
+++ community-website/src/components/server-list.tsx
@@ -1,41 +1,28 @@
-'use client';
-
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
 import Image from 'next/image';
+import Link from 'next/link';
 import cardbg from '/public/pl-enclosure.jpg';
 import tf2logo from '/public/tf2logo.png';
 
-export default function ServerPage() {
-  const [serversInfo, setServersInfo] = useState([]);
-
-  useEffect(() => {
-    async function fetchServersInfo() {
-      const response = await fetch('/api/servers');
-      const data = await response.json();
-      console.log('Servers info:', data); // Log servers info for debugging
-      setServersInfo(data);
-    }
-
-    fetchServersInfo();
-  }, []);
-
+export default function ServerList({ serversInfo = [] }) {
   return (
     <div className="flex 2xl:w-1/2 lg:w-3/5 md:3/4 container justify-center bg-dark py-20">
       <div className="gap-4 basis-2/3">
         {serversInfo.length > 0 ? (
           serversInfo.map((serverInfo, index) => (
-            <div className="flex flex-col p-2 bg-darker drop-shadow-xl" key={index}>
+            <div
+              className="relative flex flex-col rounded p-2 bg-darker drop-shadow-xl overflow-hidden group"
+              key={index}
+            >
               <Image
                 src={cardbg}
                 alt="serverCard_bg"
                 layout="fill"
                 objectFit="cover"
-                className="brightness-75"
+                className="brightness-75 rounded-lg transition-all duration-300 group-hover:brightness-50"
               />
               <div className="flex justify-between relative">
                 <div className="server-info py-2 pl-4">
-                  <h2 className="text-2xl pb-2">[FIN] Pelitupa | Payload</h2>
+                  <h2 className="text-2xl pb-2">{serverInfo.name}</h2>
                   <p className="text-1xl">Kartta: {serverInfo.map}</p>
                   <p className="text-1xl">Pelaajat: {serverInfo.players}/24</p>
                 </div>
@@ -45,7 +32,13 @@ export default function ServerPage() {
                     {serverInfo.online ? 'Online' : 'Offline'}
                   </p>
                 </div>
-                
+              </div>
+              <div
+                className="absolute inset-0 z-20 flex items-center justify-center bg-black bg-opacity-50 text-white text-xl font-bold opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+              >
+              <Link className="bg-red p-1 px-4 rounded-md" href={`steam://connect/${serverInfo.ip}:${serverInfo.port}`}>
+                Liity
+              </Link>
               </div>
             </div>
           ))
