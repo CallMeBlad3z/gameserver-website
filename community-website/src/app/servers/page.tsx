32a7e8beaf9feb70e0d2fc326918c678b7a0f820
Blad3z <santtu366@live.com>
2025-02-24 19:34:02 +0200

    Fetch server information asynchronously and pass data to ServerList component

--- community-website/src/app/servers/page.tsx
+++ community-website/src/app/servers/page.tsx
@@ -1,8 +1,20 @@
 import ServerList from '@/components/server-list';
-export default function ServerPage() {
+
+export default async function ServerPage() {
+  let serversInfo = [];
+  try {
+    const response = await fetch('http://localhost:3000/api/servers');
+    serversInfo = await response.json();
+  } catch (error) {
+    console.error('Error fetching server info:', error);
+  }
+
   return (
-    <div className="bg-red">
-      <ServerList />
+    <div className="bg-darker text-white pt-40">
+      <h1 className="text-4xl text-center text-white mb-28">Palvelimet</h1>
+      <div className="flex justify-center">
+        <ServerList serversInfo={serversInfo} />
+      </div>
     </div>
   );
 }
