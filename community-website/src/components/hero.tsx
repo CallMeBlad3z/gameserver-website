7167044c3dc8baa54596c2fd4ed6597a8156176e
Blad3z <santtu366@live.com>
2025-03-04 23:34:55 +0200

    Refactor Hero component layout and styling

--- community-website/src/components/hero.tsx
+++ community-website/src/components/hero.tsx
@@ -1,7 +1,6 @@
-'use client'; 
+'use client';
 
 import { useState, useEffect } from 'react';
-import type { StaticImageData } from "next/image";
 import Image from "next/image";
 
 interface HeroProps {
@@ -10,6 +9,7 @@ interface HeroProps {
     imgAlt: string; 
     title: string;
 }
+
 // This is the Hero component. It is displayed at the top of the page and contains a title and a background image that changes every 5 seconds.
 export default function Hero(props: HeroProps) { 
     const [opacity1, setOpacity1] = useState(1); // Set the opacity of the first image to 1
@@ -28,29 +28,26 @@ export default function Hero(props: HeroProps) {
         return () => clearInterval(timer);      // Clear the interval when the component is unmounted
     }, [opacity1]);
 
-return (
-    <div className="flex items-center justify-center relative pointer-events-none h-dvh w-dvh">
-        <div className="absolute -z-10 inset-0">
-            <div className="absolute layout-fill h-full w-full ease-in transition-opacity duration-1000 opacity-100">
-                <Image 
+    return (
+        <div className="flex mt-16 text-white items-center justify-center mx-auto">
+            <div className="relative 2xl:w-3/5 lg:w-3/4 h-96 sm:h-80 md:h-96 lg:h-128 xl:h-160 2xl:h-192 rounded-2xl overflow-hidden">
+                <Image
                     src={props.imgData1}
                     alt={props.imgAlt}
-                    layout="fill"
-                    objectFit="cover"
+                    className="absolute inset-0 w-full h-full object-cover blur-[2px] transition-opacity brightness-[40%] duration-1000"
+                    style={{ opacity: opacity1 }}
                 />
-            </div>
-            <div className={`absolute h-full w-full ease-in transition-opacity duration-1000 ${opacity2 === 1 ? 'opacity-100' : 'opacity-0'}`}>
-                <Image 
+                <Image
                     src={props.imgData2}
                     alt={props.imgAlt}
-                    layout="fill"
-                    objectFit="cover"
+                    className="absolute inset-0 w-full h-full object-cover blur-[2px] transition-opacity brightness-[40%] duration-1000"
+                    style={{ opacity: opacity2 }}
                 />
-            </div>
-                <div className="absolute inset-0 bg-gradient-to-r from-slate-700"></div>
-            </div>
-            <div className="pt-48 flex justify-center items-center">
-                <h1 className="text-white text-6xl">{props.title}</h1>
+                <div className="absolute inset-0 flex flex-col items-start justify-center pl-8">
+                    <h1 className="text-8xl font-bold bg-gradient-to-r from-red-custom from-15% via-purple-500 via-50% to-blue-custom to-85% bg-clip-text text-transparent">{props.title}</h1>
+                    <p className="text-5xl">Suomalainen peliyhteisö vuodesta 2020.</p>
+                </div>
+                
             </div>
         </div>
     );
