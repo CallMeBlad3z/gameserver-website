914308e0454875885ceb0b5a32ffb2c1b18e3a55
Blad3z <santtu366@live.com>
2025-03-06 21:56:01 +0200

    Refactor Hero component for responsive design and improved text styling

--- community-website/src/components/hero.tsx
+++ community-website/src/components/hero.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import Image from "next/image";
+import { GeistMono } from 'geist/font/mono';
 
 interface HeroProps {
     imgData1: StaticImageData; // The first image displayed in the background
@@ -30,7 +31,7 @@ export default function Hero(props: HeroProps) {
 
     return (
         <div className="flex mt-16 text-white items-center justify-center mx-auto">
-            <div className="relative 2xl:w-3/5 lg:w-3/4 h-96 sm:h-80 md:h-96 lg:h-128 xl:h-160 2xl:h-192 rounded-2xl overflow-hidden">
+            <div className="relative 2xl:basis-3/5 xl:basis-8/12 md:w-10/12 sm:w-11/12 h-96 sm:h-96 md:h-128 xl:h-160 2xl:h-192 rounded-2xl overflow-hidden">
                 <Image
                     src={props.imgData1}
                     alt={props.imgAlt}
@@ -43,9 +44,9 @@ export default function Hero(props: HeroProps) {
                     className="absolute inset-0 w-full h-full object-cover blur-[2px] transition-opacity brightness-[40%] duration-1000"
                     style={{ opacity: opacity2 }}
                 />
-                <div className="absolute inset-0 flex flex-col items-start justify-center pl-8">
-                    <h1 className="text-8xl font-bold bg-gradient-to-r from-red-custom from-15% via-purple-500 via-50% to-blue-custom to-85% bg-clip-text text-transparent">{props.title}</h1>
-                    <p className="text-5xl">Suomalainen peliyhteisö vuodesta 2020.</p>
+                <div className="absolute inset-0 flex flex-col xl:items-start sm:items-center justify-center xl:pl-8">
+                    <h1 className="2xl:text-8xl xl:text-7xl md:text-6xl font-bold bg-gradient-to-r from-red-custom from-15% via-purple-500 via-50% to-blue-custom to-85% bg-clip-text text-transparent">{props.title}</h1>
+                    <p className="2xl:text-5xl xl:text-4xl md:text-3xl">Suomalainen peliyhteisö vuodesta <span className={`${GeistMono.className}`}>2018</span>.</p>
                 </div>
                 
             </div>
